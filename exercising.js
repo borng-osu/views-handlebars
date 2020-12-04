88566c9423f694bfd7904839e12a8a94490b52a1
borng-osu <62971727+borng-osu@users.noreply.github.com>
2020-12-04 18:56:33 -0500

    HW6?

---
+++ exercising.js
@@ -0,0 +1,167 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+  host: "mysql.eecs.oregonstate.edu",
+  user: "cs290_borng",
+  password: "",
+  database: "cs290_borng"
+})
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 7363);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+app.get('/',function(req,res,next){
+    var context = {};
+    pool.query('SELECT * FROM workouts', function(err,rows,fields){
+        if(err){
+            next(err);
+            return;
+        }
+        var sqlInfo = [];
+        for (var row in rows) {
+            sqlInfo.push({
+                'id':rows[row].id,
+                'name':rows[row].name,
+                'reps':rows[row].reps,
+                'weight':rows[row].weight,
+                'date':rows[row].date,
+                'lbs':rows[row].lbs
+            });
+        }
+        context.routine = sqlInfo;
+        res.render('home', context);
+    });
+});
+
+app.get('/reset-table',function(req,res,next){
+    var context = {};
+    pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+      var createString = "CREATE TABLE workouts("+
+      "id INT PRIMARY KEY AUTO_INCREMENT,"+
+      "name VARCHAR(255) NOT NULL,"+
+      "reps INT,"+
+      "weight INT,"+
+      "date DATE,"+
+      "lbs BOOLEAN)";
+      pool.query(createString, function(err){
+        context.results = "Table reset";
+        res.render('home',context);
+      })
+    });
+  });
+
+app.get('/insert',function(req,res,next){
+  var context = {};
+  pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?, ?, ?, ?, ?)", 
+  [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.routine = result.insertId;
+    res.send(JSON.stringify(context));
+  });
+});
+
+app.get('/edit',function(req,res,next){
+    var context = {};
+    pool.query('SELECT * FROM workouts WHERE id=?', [req.query.id], function(err,rows,fields){
+        if(err){
+            next(err);
+            return;
+        }
+        var sqlInfo = [];
+        sqlInfo.push({
+            'id':rows[0].id,
+            'name':rows[0].name,
+            'reps':rows[0].reps,
+            'weight':rows[0].weight,
+            'date':rows[0].date,
+            'lbs':rows[0].lbs
+        });
+        context.routine = sqlInfo;
+        res.render('edit', context);
+    });
+});
+
+
+app.get('/safe-update',function(req,res,next){
+    var context = {};
+    pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, result){
+      if(err){
+        next(err);
+        return;
+      }
+      if(result.length == 1){
+        var curVals = result[0];
+        mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
+          [req.query.name || curVals.name, req.query.reps || curVals.reps, 
+            req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.lbs || curVals.lbs, req.query.id],
+          function(err, result){
+          if(err){
+            next(err);
+            return;
+          }
+         
+        pool.query('SELECT * FROM workouts', function(err,rows,fields){
+            if(err){
+                next(err);
+                return;
+            }
+            var sqlInfo = [];
+            for (var row in rows) {
+                sqlInfo.push({
+                    'id':rows[row].id,
+                    'name':rows[row].name,
+                    'reps':rows[row].reps,
+                    'weight':rows[row].weight,
+                    'date':rows[row].date,
+                    'lbs':rows[row].lbs
+                });
+            }
+            context.routine = sqlInfo;
+            res.render('home', context);
+        });
+          
+        });
+      }
+    });
+});
+
+app.get('/delete',function(req,res,next){
+    pool.query("DELETE FROM workouts WHERE id = ?", 
+    [req.query.id], function(err, result){
+      if(err){
+        next(err);
+        return;
+      }
+    });
+});
+  
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
---
+++ script.js
@@ -0,0 +1,63 @@
+let insertion = document.getElementById('insert');
+
+insertion.getElementById('add').addEventListener('click',function(event){
+    e.preventDefault();
+    
+    let req = new XMLHttpRequest();
+    let data = "name="+insertion.elements.name.value+ "&reps="+insertion.elements.reps.value+"&weight="+insertion.elements.weight.value+"&date="+insertion.elements.weight.value+"&lbs="+insertion.elements.lbs.value;
+    
+    req.open("GET", "/insert?" + data, true);
+    req.setRequestHEader('Content-Type', 'application/x-www-form-urlencoded');
+
+    req.addEventListener('load', function(){
+        if(req.status >= 200 && req.status < 400){
+            let received = JSON.parse(req.responseText);
+            let id = received.routine;
+            let table = document.getElementById('content');
+            let new_row = table.insertRow(-1);
+            let td_info = [insertion.elements.name.value, insertion.elements.reps.value, insertion.elements.weight.value, insertion.elements.date.value, insertion.elements.lbs.value]
+            for (let i = 0; i < 5; i++) {
+                let addition = document.createElement('td');
+                addition.textContent = td_info[i]
+                new_row.appendChild(addition);
+            }
+            let options = document.createElement('td');
+            let deletion = document.createElement('input');
+            let editor = document.createElement('a');
+            editor.setAttribute('href', '/edit?id=' + id);
+            editor.textContent = "Edit";
+            deletion.setAttribute('type', 'button');
+            deletion.setAttribute('value', 'Delete');
+            deletion.setAttribute('onClick', 'deleteRow(' + id + ')');
+        } else {
+            console.log("There was an error, idiot.");
+        } 
+    });
+    req.send("/insert?" + data);
+});
+
+function deleteRow(id) {
+    let table = document.getElementById('content');
+    let count = tables.rows.length;
+
+    for (let i = 1; i < count; i++){
+        let cur = table.rows[i]
+        if (cur.id == id) {
+            table.deleteRow(i);
+        }
+    }
+
+    let req = new XMLHttpRequest();
+
+    req.open("GET", "/delete?id=" + id, true);
+
+    req.addEventListener("load", function(){
+        if(req.status >= 200 && req.status < 400) {
+            console.log("successful deletion");
+        } else {
+            console.log("there was an error, idiot.");
+        }
+    });
+
+    req.send("/delete?id=" + id);
+}
